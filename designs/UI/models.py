9c0632b5a711b0850c7231bd8669d136893b3c8b
paulusk24 <paulusk24@gmail.com>
2018-02-21 11:53:27 +0300

    Update of the classes

--- designs/UI/models.py
+++ designs/UI/models.py
@@ -1,5 +1,5 @@
 #User Models
-class users():
+class Users(object):
        
     def __init__(self,username,email, password,user_date_stamp):
         #initiliazing User class constructor
@@ -10,10 +10,10 @@ class users():
 
     
 
-class businesses():
+class Businesses(object):
     
     def __init__(self, business_id, business_title,business_description,category_id,email,business_date_stamp):
-        #initiliazing recipe class constructor
+        #initiliazing business class constructor
         self.business_id= business_id
         self.business_title = business_title
         self.business_description= business_description
@@ -21,4 +21,11 @@ class businesses():
         self.email= email
         self.business_date_stamp = business_date_stamp
 
-   
+class Reviews(object):
+
+    def __init__(self,username,review_id,review_description,business_id):
+     #initiliazing business reviews constructor
+     self.username=username
+     self.review_id= review_id
+     self.review_description= review_description
+     self.business_id= business_id
